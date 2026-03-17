41154b2595887b47620182ba297b4dad5e9e014c
Shiva-Sai-Krishna-0408 <kesanupalli.saikrishna@gmail.com>
2026-03-17 13:35:56 -0700

    Add Streamlit dashboard with interactive risk map

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import folium
+import pandas as pd
+from streamlit_folium import st_folium
+
+def get_color(risk_score):
+    return "green" if risk_score <= 25 else "orange" if risk_score > 25 and risk_score <= 50 else "red" if risk_score > 50 and risk_score <= 75 else "darkred"
+
+st.title("Climate Risk Intelligence System - India")
+df = pd.read_csv(r"C:\Users\Shiva\Projects\climate-risk-intelligence\data\india_features_v2.csv")
+st.dataframe(df)
+
+india_map = folium.Map(location=[20.5937,78.9629],zoom_start=5)
+
+
+
+for _,row in df.iterrows():
+    folium.CircleMarker(
+        location = [row["latitude"],row["longitude"]],
+        radius = 10,
+        color = get_color(row["risk_score"]),
+        fill = True,
+        fill_color = get_color(row["risk_score"]),
+        fill_opacity=0.6,
+        popup=f"{row['name']} | Risk Score: {row['risk_score']}",
+        tool_tip="Click me"
+    ).add_to(india_map)
+
+st_folium(india_map, width=700)
